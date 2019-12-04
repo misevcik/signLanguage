d75cb4a0fdb455fcb91bd8accdd583d1d5fd2f6e
Mike <michal.sevcik@soft-art.sk>
2019-12-04 05:58:23 +0100

    Merge branch 'master' of https://github.com/misevcik/signLanguage
    # Conflicts:
    #	Android/app/src/main/res/layout/fragment_test_detail.xml

--- Android/app/src/main/java/sk/doreto/signlanguage/ui/components/TestAnswerControllerView.java
+++ Android/app/src/main/java/sk/doreto/signlanguage/ui/components/TestAnswerControllerView.java
@@ -1,12 +1,14 @@
 package sk.doreto.signlanguage.ui.components;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import sk.doreto.signlanguage.R;
 import sk.doreto.signlanguage.ui.education.QuestionItem;
@@ -16,7 +18,9 @@ public class TestAnswerControllerView extends LinearLayout {
 
     private TextView quistionText;
     private ArrayList<TestAnswerItem> answerItems = new ArrayList<TestAnswerItem>(3);
-
+    private List<QuestionItem> mQuestions;
+    private ArrayList<QuestionItem> mAnswered = new ArrayList<>();
+    private Handler handler = new Handler();
 
     public TestAnswerControllerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,11 +28,27 @@ public class TestAnswerControllerView extends LinearLayout {
 
     }
 
-    public void fillData(QuestionItem questionItem) {
+    public void callbackFinished(){
+
+    }
+
+    public void fillData(List<QuestionItem> questions){
+        if (!questions.isEmpty()) {
+            mQuestions = questions;
+            QuestionItem questionItem = mQuestions.remove(0);
+            mAnswered.clear();
+            mAnswered.add(questionItem);
+            fillData(questionItem);
+        }
+    }
+
+    private void fillData(QuestionItem questionItem) {
 
         int index = 0;
         for (QuestionItem.AnswerItem item : questionItem.answerCollection) {
-            answerItems.get(index++).setAnswer(item.answerText);
+            answerItems.get(index).reset();
+            answerItems.get(index).setValue(item);
+            ++index;
         }
     }
 
@@ -49,7 +69,18 @@ public class TestAnswerControllerView extends LinearLayout {
             item.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    ((TestAnswerItem) v).clicked();
+                    TestAnswerItem clickedItem = ((TestAnswerItem) v);
+                    clickedItem.clicked();
+                        //update statistics
+                        //set timer to show next question
+                    if (!mQuestions.isEmpty()) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                fillData(mQuestions.remove(0));
+                            }
+                        }, 1000);
+                    }
                 }
             });
         }
--- Android/app/src/main/java/sk/doreto/signlanguage/ui/components/TestAnswerItem.java
+++ Android/app/src/main/java/sk/doreto/signlanguage/ui/components/TestAnswerItem.java
@@ -6,11 +6,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import sk.doreto.signlanguage.R;
+import sk.doreto.signlanguage.ui.education.QuestionItem;
 
 public class TestAnswerItem extends LinearLayout {
 
     private TextView answerText;
     private LinearLayout layout;
+    private boolean mIsCorect = false;
 
     public TestAnswerItem(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -22,7 +24,7 @@ public class TestAnswerItem extends LinearLayout {
     }
 
     public void clicked() {
-        layout.setBackgroundResource(R.drawable.test_answer_round_gray);
+        layout.setBackgroundResource(mIsCorect? R.drawable.test_answer_round_gray : R.drawable.test_answer_round_red);
     }
 
     private void init(Context context, AttributeSet attrs) {
@@ -37,7 +39,9 @@ public class TestAnswerItem extends LinearLayout {
 
     }
 
-    public void setAnswer(String answerText) {
-        this.answerText.setText(answerText);
+    public void setValue(QuestionItem.AnswerItem item){
+        this.answerText.setText(item.answerText);
+        this.mIsCorect = item.isCorrect;
     }
+    public boolean isCorrect(){return mIsCorect;}
 }
